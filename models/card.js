636031ce18c038d700f78b349178d1c45e75e051
Tim <tim@192.168.0.101>
2022-07-13 19:24:27 +0300

    add user&card models | Project version 13.0.0

--- models/card.js
+++ models/card.js
@@ -1,6 +1,28 @@
-// Поля схемы карточки:
-// name — имя карточки, строка от 2 до 30 символов, обязательное поле;
-// link — ссылка на картинку, строка, обязательно поле.
-// owner — ссылка на модель автора карточки, тип ObjectId, обязательное поле;
-// likes — список лайкнувших пост пользователей, массив ObjectId, по умолчанию — пустой массив (поле default);
-// createdAt — дата создания, тип Date, значение по умолчанию Date.now.
+const mongoose = require('mongoose');
+
+const cardSchema = new mongoose.Schema({
+  name: {
+    type: String, // строка
+    required: true, // обязательное поле
+    minlength: 2, // минимальная длина имени — 2 символа
+    maxlength: 30, // а максимальная — 30 символов
+  },
+  link: {
+    type: String, // строка
+    required: true, // обязательное поле
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId, // ObjectId
+    required: true, // обязательное поле
+  },
+  likes: [{
+    type: mongoose.Schema.Types.ObjectId, // ObjectId
+    default: [], // пустой массив
+  }],
+  createdAt: {
+    type: Date, // дата
+    default: Date.now(), // значение по умолчанию
+  },
+});
+
+module.exports = mongoose.mongo.model('user', cardSchema);
